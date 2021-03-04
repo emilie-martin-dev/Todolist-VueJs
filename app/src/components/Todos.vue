0c2f7810d14349ca7f1214519023c5f3cd2b31c2
Justine Martin <justine.martin.dev@gmail.com>
2021-03-04 20:21:28 +0100

    feat : Adapte le code existant a la nouvelle structure de donnée fix : Cliquer sur le label d'une checkbox coche maintenant la bonne todo

--- app/src/components/Todos.vue
+++ app/src/components/Todos.vue
@@ -1,119 +1,125 @@
 <template>
-	<h1> TodoList </h1>
-	<ul>
-		<li v-for="todo in filteredTodos" :key="todo.name" >
-			<input type="checkbox" id="checkbox" v-model=todo.completed>
-			<label for="checkbox">{{todo.id}} | Nom de la tache: {{todo.name}} - Completer:{{ (todo.completed == false ? 'non' : 'oui')}} </label>
+	<div>
+		<h1>TodoList</h1>
+		<ul>
+			<li v-for="todo in filteredTodos" :key="todo.name">
+				<input type="checkbox" :id="'checkbox-' + todo.id" v-model=todo.completed>
+				<label :for="'checkbox-' + todo.id">{{todo.id}} | Nom de la tache: {{todo.name}} - Completer:{{ (todo.completed == false ? 'non' : 'oui')}} </label>
 
-			<button v-on:click="decrease(todo)" >Supprimer la todo</button>
-			<button v-on:click="update(todo)" >Modifier la todo</button>
-			<input type="texte" id="addTodoName" v-model='addTodoName' placeholder="Nom de la tache">
-		</li>
-	</ul>
+				<button v-on:click="decrease(todo)" >Supprimer la todo</button>
+				<button v-on:click="update(todo)" >Modifier la todo</button>
+				<input type="texte" id="addTodoName" v-model='addTodoName' placeholder="Nom de la tache">
+			</li>
+		</ul>
 
-	<input type="texte" id="addTodoName" v-model='addTodoName' placeholder="Nom de la tache">
-	<label> Tache completer: </label>
-	<input type="checkbox" id="addTodoCompleted" v-model="addTodoCompleted">
+		<input type="texte" id="addTodoName" v-model='addTodoName' placeholder="Nom de la tache">
+		<label> Tache completer: </label>
+		<input type="checkbox" id="addTodoCompleted" v-model="addTodoCompleted">
 
 
-	<button v-on:click="ajouter" >Ajouter une todo</button>
+		<button v-on:click="ajouter">Ajouter une todo</button>
 
-	<button v-bind:id="complete" v-on:click.prevent="filter = 'todo' ">tache compléter</button>
-	<button v-bind:id="cour" v-on:click.prevent="filter = 'done' ">tache en cours</button>
-	<button v-bind:id="all" v-on:click.prevent="filter = 'all' ">toute tacher</button>
+		<button v-on:click.prevent="filter = 'done' ">tache complétées</button>
+		<button v-on:click.prevent="filter = 'todo' ">tache en cours</button>
+		<button v-on:click.prevent="filter = 'all' ">toute tacher</button>
+	</div>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
-export default defineComponent({
-	data() {
-		return {
-			todos: [
-				{
-					id: 1,
-					name : 'boucler todo dans une liste',
-					completed : true
-				},
-				{
-					id: 2,
-					name : 'checkbox marque todo completed',
-					completed: true
-				},
-				{
-					id: 3,
-					name : 'supprimer la todo (mais pas avec slice() :/)',
-					completed: true
-				},
-				{
-					id: 4,
-					name : 'filtrer les todos affichées avec computed',
-					completed: false
-				},
-				{
-					id: 5,
-					name : 'filtrer dans le template avec des boutons',
-					completed: false
-				},
-				{
-					id: 6,
-					name : 'modifier la source des données affichées dans le template',
-					completed: false
-				},
-				{
-					id: 7,
-					name : 'ajouter une todo',
-					completed: true
-				},
-				{
-					id: 8,
-					name : 'déplacer la gestion des données dans un store Vuex',
-					completed: false
-				}
-			],
-			addTodoCompleted: '',
-			addTodoName:'',
-			filter: 'all',
+	import { defineComponent } from 'vue';
+	export default defineComponent({
+		data() {
+			return {
+				todolist: [
+					{
+						id: 1,
+						todos: [
+							{
+								id: 1,
+								name : 'boucler todo dans une liste',
+								completed : true
+							},
+							{
+								id: 2,
+								name : 'checkbox marque todo completed',
+								completed: true
+							},
+							{
+								id: 3,
+								name : 'supprimer la todo (mais pas avec slice() :/)',
+								completed: true
+							},
+							{
+								id: 4,
+								name : 'filtrer les todos affichées avec computed',
+								completed: false
+							},
+							{
+								id: 5,
+								name : 'filtrer dans le template avec des boutons',
+								completed: false
+							},
+							{
+								id: 6,
+								name : 'modifier la source des données affichées dans le template',
+								completed: false
+							},
+							{
+								id: 7,
+								name : 'ajouter une todo',
+								completed: true
+							},
+							{
+								id: 8,
+								name : 'déplacer la gestion des données dans un store Vuex',
+								completed: false
+							}
+						]
+					}
+				],
+				addTodoCompleted: '',
+				addTodoName:'',
+				filter: 'all',
 			}
 		},
-
-	methods: {
-		decrease: function(index){
-			this.todos.splice(this.todos.indexOf(index),1);
-		},
-		ajouter(){
-			this.todos.push(
-				{
-					id:(this.todos.length+1),
-					name:this.addTodoName.toString(),
-					completed: (this.addTodoCompleted == true ? true : false)
-				}
-			);
-		},
-		update: function(index){
-			this.todos.splice(this.todos.indexOf(index),1);
-		}
-	},
-	props: {
-
-	},
-	computed:{
-		remaining(){
-			return this.todos.filter(function (todo){
-				return !todo.completed
-			}).length
-		},
-		hasTodos(){
-			return this.todos.length > 0
-		},
-		filteredTodos(){
-			if(this.filter === 'todo'){
-				return this.todos.filter(todo => todo.completed)
-			} else if (this.filter === 'done'){
-				return this.todos.filter(todo => !todo.completed)
+		methods: {
+			decrease: function(index){
+				this.todolist[0].todos.splice(this.todolist[0].todos.indexOf(index),1);
+			},
+			ajouter(){
+				this.todolist[0].todos.push(
+					{
+						id:(this.todolist[0].todos.length+1),
+						name:this.addTodoName.toString(),
+						completed: (this.addTodoCompleted == true ? true : false)
+					}
+				);
+			},
+			update: function(index){
+				this.todolist[0].todos.splice(this.todolist[0].todos.indexOf(index),1);
 			}
+		},
+		props: {
 
-			return this.todos
+		},
+		computed:{
+			remaining(){
+				return this.todolist[0].todos.filter(function (todo){
+					return !todo.completed
+				}).length
+			},
+			hasTodos(){
+				return this.todolist[0].todos.length > 0
+			},
+			filteredTodos(){
+				if(this.filter === 'todo'){
+					return this.todolist[0].todos.filter(todo => !todo.completed)
+				} else if (this.filter === 'done'){
+					return this.todolist[0].todos.filter(todo => todo.completed)
+				}
+
+				return this.todolist[0].todos
+			}
 		}
-	}
-});
+	});
 </script>
