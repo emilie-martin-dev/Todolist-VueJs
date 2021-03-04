8d8eac2ea37046f83f9858d3b2cdd3a4edcfa63d
Paul Lebranchu <21403460@etu.unicaen.fr>
2021-03-04 16:27:47 +0100

    feat: initilaisation du projet

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+
+  <Todos />
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Todos
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ app/src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Todos from '../components/Todos.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Todos
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+
+
+export default router
