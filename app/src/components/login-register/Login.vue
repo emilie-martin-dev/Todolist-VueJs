e18a37835b527b5f9df267ea2f9dae97ababfcd4
Justine Martin <justine.martin.dev@gmail.com>
2021-03-11 14:26:05 +0100

    ui : Design de la page de connexion

--- app/src/components/login-register/Login.vue
+++ app/src/components/login-register/Login.vue
@@ -1,5 +1,27 @@
 <template>
-	<p>Login</p>
+	<div class="row">
+		<form class="col s12">
+			<div class="row">
+				<div class="input-field col s12">
+					<input placeholder="Nom d'utilisateur" id="username" type="text" class="validate" v-model="username">
+					<label for="username">Nom d'utilisateur</label>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="input-field col s12">
+					<input placeholder="Mot de passe" id="password" type="password" class="validate" v-model="password">
+					<label for="password">Mot de passe</label>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="col s12 center-align">
+					<a class="waves-effect waves-light btn">Connexion</a>
+				</div>
+			</div>
+		</form>
+	</div>
 </template>
 
 <script>
@@ -10,15 +32,12 @@
 		
 		data () {
 			return {
-				
+				username: "",
+				password: ""
 			}
 		},
 
 		methods: {
-
-		},
-
-		computed:{
 
 		}
 
--- app/src/view/LoginRegisterVue.vue
+++ app/src/view/LoginRegisterVue.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="container row">
-		<div class="col s12">
+		<div class="col s12 m6 offset-l3">
 			<login v-show="isLoginUi"></login>
 			<register v-show="!isLoginUi"></register>
 		</div>
