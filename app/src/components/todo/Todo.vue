265de9aed5f964dd7caed7c7ced0f9cc1a0803e4
Justine Martin <justine.martin.dev@gmail.com>
2021-03-18 18:53:30 +0100

    fix : N'affiche plus d'erreur dans le cas où il n'y a plus de liste

--- app/src/components/todo/Todo.vue
+++ app/src/components/todo/Todo.vue
@@ -1,31 +1,33 @@
 <template>
-	<h1 v-show="!showInput">
-		<span v-on:click="transformTodo(selectedList)">{{todolist[selectedList].name}}</span>
-		<a class=" btn-flat"><i v-on:click="deleteListTodo({list: todolist[selectedList]})" class="material-icons">delete</i></a>
-	</h1>
-	<h1 v-show="showInput"><input type='text' :value="todolist[selectedList].name" v-on:keyup.enter="transformTodo(todolist[selectedList].id)" id="todoListName"/> </h1>
+	<div v-if="selectedList in todolist">
+		<h1 v-show="!showInput">
+			<span v-on:click="transformTodo(selectedList)">{{todolist[selectedList].name}}</span>
+			<a class=" btn-flat"><i v-on:click="deleteListTodo({list: todolist[selectedList]})" class="material-icons">delete</i></a>
+		</h1>
+		<h1 v-show="showInput"><input type='text' :value="todolist[selectedList].name" v-on:keyup.enter="transformTodo(todolist[selectedList].id)" id="todoListName"/> </h1>
 
-	<div class="row" v-for="todo in filteredTodosArg" :key="todo.name">
-		<label class="col s1"><input type="checkbox" :id="'checkbox-' + todo.id" v-model=todo.completed><span></span></label>
-		<div class="col s10">
-			<span v-show='number!=todo.id' v-on:click="transform(selectedList, todo)">{{todo.name}}</span>
-			<input v-show='number==todo.id' v-on:keyup.enter="transform(selectedList, todo)"  type='text' :value="todo.name" v-bind:id="todo.id"/>
+		<div class="row" v-for="todo in filteredTodosArg" :key="todo.name">
+			<label class="col s1"><input type="checkbox" :id="'checkbox-' + todo.id" v-model=todo.completed><span></span></label>
+			<div class="col s10">
+				<span v-show='number!=todo.id' v-on:click="transform(selectedList, todo)">{{todo.name}}</span>
+				<input v-show='number==todo.id' v-on:keyup.enter="transform(selectedList, todo)"  type='text' :value="todo.name" v-bind:id="todo.id"/>
+			</div>
+			<a class="col s1 btn-flat"><i v-on:click="deleteTodo({listIndex: selectedList, todo: todo})" class="material-icons">delete</i></a>
 		</div>
-		<a class="col s1 btn-flat"><i v-on:click="deleteTodo({listIndex: selectedList, todo: todo})" class="material-icons">delete</i></a>
-	</div>
 
-	<div class="row">
-		<div class="input-field col s6">
-			<input placeholder="Ajouter une tâche" id="newTodoName" type="text" v-model="newTodoName"/>
+		<div class="row">
+			<div class="input-field col s6">
+				<input placeholder="Ajouter une tâche" id="newTodoName" type="text" v-model="newTodoName"/>
+			</div>
+			<a class="col 3 waves-effect waves-light btn" v-on:click="ajouter({listIndex: selectedList, name: newTodoName}); newTodoName=''"><i class="material-icons left">add</i>Ajouter</a>
 		</div>
-		<a class="col 3 waves-effect waves-light btn" v-on:click="ajouter({listIndex: selectedList, name: newTodoName}); newTodoName=''"><i class="material-icons left">add</i>Ajouter</a>
-	</div>
 
-	<div class="row">
-		<a class="btn-flat" v-on:click.prevent="filter = 'all'" v-bind:class="[filter == 'all' ? 'disabled': '']">Toutes</a>
-		<a class="btn-flat" v-on:click.prevent="filter = 'todo'" v-bind:class="[filter == 'todo' ? 'disabled': '']">A faire</a>
-		<a class="btn-flat" v-on:click.prevent="filter = 'done'" v-bind:class="[filter == 'done' ? 'disabled': '']">Faites</a>
-		<span>Il reste {{remainingCurrentList}} tâches à faire</span>
+		<div class="row">
+			<a class="btn-flat" v-on:click.prevent="filter = 'all'" v-bind:class="[filter == 'all' ? 'disabled': '']">Toutes</a>
+			<a class="btn-flat" v-on:click.prevent="filter = 'todo'" v-bind:class="[filter == 'todo' ? 'disabled': '']">A faire</a>
+			<a class="btn-flat" v-on:click.prevent="filter = 'done'" v-bind:class="[filter == 'done' ? 'disabled': '']">Faites</a>
+			<span>Il reste {{remainingCurrentList}} tâches à faire</span>
+		</div>
 	</div>
 </template>
 
@@ -78,7 +80,7 @@
 				store.commit("todolist/deleteListTodo", {list :list});
 			}
 		},
-
+		
 		props: {
 			idListTodo: {
 				type: Number,
